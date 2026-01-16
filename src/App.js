2a35d254083c76af8bc3805d71c3e0950348b328
Himanshu Yadav <himanshukoshliya51@gmail.com>
2026-01-16 18:41:19 +0530

    Initial commit: Netflix GPT React app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import './App.css';
+import "./index.css";
+
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-800" >
+      NAMASTE EVERYONE LETS BUILD NETFILX GPT
+    </div>
+  );
+}
+
+export default App;
