d7ded148664c6a3d0124bcca4ba762240647262b
Himanshu Yadav <himanshukoshliya51@gmail.com>
2026-01-19 09:13:09 +0530

    same as prev little fix

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,104 +1,149 @@
-import React, { useRef, useState } from 'react'
-import Header from './Header'
-import { checkValidData } from '../utils/validate';
-import { createUserWithEmailAndPassword , signInWithEmailAndPassword, updateProfile} from "firebase/auth";
-import { auth } from '../utils/firebase';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { addUser } from '../utils/userSlice';
-
+import React, { useRef, useState } from "react";
+import Header from "./Header";
+import { checkValidData } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Login = () => {
-  const[isSignInForm,setisSignInForm] = useState(true);
-  const[errorMessage,seterrorMessage] = useState(null);
+  const [isSignInForm, setisSignInForm] = useState(true);
+  const [errorMessage, seterrorMessage] = useState(null);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  
+
   const name = useRef(null);
   const email = useRef(null);
   const password = useRef(null);
 
-
   const handleButtonClick = () => {
     //Validate the form data
-    const message = checkValidData(email.current.value,password.current.value);
+    const message = checkValidData(email.current.value, password.current.value);
     seterrorMessage(message);
 
-    if(message) return;
-    
+    if (message) return;
 
     //sign in /sign up
-    if(!isSignInForm){
+    if (!isSignInForm) {
       //Sign up logic
-      createUserWithEmailAndPassword(auth,email.current.value,password.current.value)
-  .then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value,
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
 
-    updateProfile(user, {
-  displayName: name.current.value, photoURL: "https://media.licdn.com/dms/image/v2/D4E03AQHNvypGHDgByw/profile-displayphoto-scale_200_200/B4EZfJ8zInHgAY-/0/1751439849935?e=1770249600&v=beta&t=8zJOUWGd8HBoAhbG39QxzLOCKGapoUpbYBKKARp5NAE",
-}).then(() => {
-  const {uid, email, displayName , photoURl}= auth.currentUser;
-          dispatch(addUser({uid:uid,email:email,displayName:displayName,photoURl:photoURl}));
-          
-  // Profile updated!
-      navigate("/browse")
-}).catch((error) => {
-  // An error occurred
-seterrorMessage(error.message)
-});
+          updateProfile(user, {
+            displayName: name.current.value,
+            photoURL:
+              "https://media.licdn.com/dms/image/v2/D4E03AQHNvypGHDgByw/profile-displayphoto-scale_200_200/B4EZfJ8zInHgAY-/0/1751439849935?e=1770249600&v=beta&t=8zJOUWGd8HBoAhbG39QxzLOCKGapoUpbYBKKARp5NAE",
+          })
+            .then(() => {
+              const { uid, email, displayName, photoURL } = auth.currentUser;
+              dispatch(
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displayName: displayName,
+                  photoURL: photoURL,
+                }),
+              );
 
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    seterrorMessage(errorCode + "-" + errorMessage);
-  });
-  }
-
-    
-    else{
+              // Profile updated!
+              navigate("/browse");
+            })
+            .catch((error) => {
+              // An error occurred
+              seterrorMessage(error.message);
+            });
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          seterrorMessage(errorCode + "-" + errorMessage);
+        });
+    } else {
       //Sign in logic
-      signInWithEmailAndPassword(auth, email.current.value,password.current.value)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    console.log(user);
-    navigate("/browse")
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    seterrorMessage(errorCode + "-" + errorMessage);
-  });     
-    }  
-   
-  }
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value,
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          navigate("/browse");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          seterrorMessage(errorCode + "-" + errorMessage);
+        });
+    }
+  };
 
   const toggleSignInForm = () => {
     setisSignInForm(!isSignInForm);
-  }
+  };
   return (
     <div>
-        <Header/>
-         <div  className='absolute'>
+      <Header />
+      <div className="absolute">
         <img
-        src="https://assets.nflxext.com/ffe/siteui/vlv3/797df41b-1129-4496-beb3-6fc2f29c59d3/web/IN-en-20260112-TRIFECTA-perspective_004732f9-7464-4a7c-940b-4a51c4f0f73f_large.jpg"
-        alt="logo"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/797df41b-1129-4496-beb3-6fc2f29c59d3/web/IN-en-20260112-TRIFECTA-perspective_004732f9-7464-4a7c-940b-4a51c4f0f73f_large.jpg"
+          alt="logo"
         />
+      </div>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="absolute left-0 right-0 w-4/12 p-12 mx-auto text-white bg-black rounded-lg bg-opacity-80 my-36"
+      >
+        <h1 className="py-4 text-3xl font-bold">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            ref={name}
+            type="text"
+            placeholder="Full Name"
+            className="w-full p-2 my-4 bg-gray-700"
+          />
+        )}
+        <input
+          ref={email}
+          type="text"
+          placeholder="Email Address"
+          className="w-full p-2 my-4 bg-gray-700"
+        />
+        <input
+          ref={password}
+          type="password"
+          placeholder="Password"
+          className="w-full p-2 my-4 bg-gray-700"
+        />
+        <p className="p-2 text-lg font-bold text-red-500">{errorMessage}</p>
+        <button
+          className="w-full p-4 my-6 bg-red-600"
+          onClick={handleButtonClick}
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign up now"
+            : "Allready registerd? Sign In now"}{" "}
+        </p>
+      </form>
     </div>
-    <form onSubmit={(e) => e.preventDefault()} className='absolute left-0 right-0 w-4/12 p-12 mx-auto text-white bg-black rounded-lg bg-opacity-80 my-36'>
-    <h1 className='py-4 text-3xl font-bold'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
-      {!isSignInForm && (<input ref={name} type="text" placeholder="Full Name"  className="w-full p-2 my-4 bg-gray-700"/>) }
-      <input ref={email} type="text" placeholder="Email Address"  className="w-full p-2 my-4 bg-gray-700"/> 
-      <input ref={password} type="password" placeholder="Password"  className="w-full p-2 my-4 bg-gray-700"/> 
-      <p className='p-2 text-lg font-bold text-red-500'>{errorMessage}</p>
-      <button className="w-full p-4 my-6 bg-red-600" onClick={handleButtonClick}>{isSignInForm ? "Sign In" : "Sign Up"}</button>
-      <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>{isSignInForm ? "New to Netflix? Sign up now" : "Allready registerd? Sign In now"} </p>
+  );
+};
 
-    </form>
-    </div> 
-  )
-}
-
-export default Login
+export default Login;
